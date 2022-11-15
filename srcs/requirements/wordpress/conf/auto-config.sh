a8b613c95a746501a551b3a970b63b921c0d88e3
aribesni <aribesni@student.42.fr>
2022-11-15 08:10:09 +0100

    trying to find where the 403 Forbidden error comes from

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -6,6 +6,10 @@ touch /run/php/php7.3-fpm.pid;
 # sleep 10;
 # mkdir /var/www/wordpress
 
+# chmod 755 /var/www/wordpress;
+
+# chown -R www-data:www-data /var/www/wordpress;
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --output-document /usr/local/bin/wp;
 
 chmod +x /usr/local/bin/wp;
@@ -14,11 +18,11 @@ mv /var/www/wp-config.php /var/www/wordpress/;
 
 cd /var/www/wordpress;
 
-wp config create    --allow-root \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306 --path='/var/www/wordpress/'
+# wp config create    --allow-root \
+#                     --dbname=$SQL_DATABASE \
+#                     --dbuser=$SQL_USER \
+#                     --dbpass=$SQL_PASSWORD \
+#                     --dbhost=mariadb:3306 --path='/var/www/wordpress/'
 
 wp core download --allow-root;
 wp core install --allow-root --url=${WP_DOMAIN_NAME} --title="inception" --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --debug --path="/var/www/wordpress/"
